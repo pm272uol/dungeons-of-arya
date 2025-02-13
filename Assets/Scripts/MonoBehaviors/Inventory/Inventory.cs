cedb06286e15539ea4a9c8c1b24464ba2d0e9686
Ouyang1887415157 <1347585006@qq.com>
2025-02-13 18:17:29 +0800

    Add comments

--- Assets/Scripts/MonoBehaviors/Inventory/Inventory.cs
+++ Assets/Scripts/MonoBehaviors/Inventory/Inventory.cs
@@ -3,17 +3,17 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    public GameObject slotPrefab;
+    public GameObject slotPrefab; // The prefab of slots
 
-    public const int numSlots = 5;
+    public const int numSlots = 5; // The number of slots 
 
-    Image[] itemImages = new Image[numSlots];
-    Item[] items = new Item[numSlots];
-    GameObject[] slots = new GameObject[numSlots];
+    Image[] itemImages = new Image[numSlots]; // The number of images
+    Item[] items = new Item[numSlots]; // The items
+    GameObject[] slots = new GameObject[numSlots]; // The slots
 
     public void Start()
     {
-        CreateSlots();
+        CreateSlots(); // Create slots
     }
 
     public void CreateSlots()
@@ -22,11 +22,15 @@ public class Inventory : MonoBehaviour
         {
             for (int i = 0; i < numSlots; i++)
             {
-                GameObject newSlot = Instantiate(slotPrefab);
-                newSlot.name = "ItemSlot_" + i;
-                newSlot.transform.SetParent(gameObject.transform.GetChild(0).transform);
-                slots[i] = newSlot;
-                itemImages[i] = newSlot.transform.GetChild(1).GetComponent<Image>();
+                GameObject newSlot = Instantiate(slotPrefab); // create a new slot from prefab
+
+                newSlot.name = "ItemSlot_" + i; // The slot name
+
+                newSlot.transform.SetParent(gameObject.transform.GetChild(0).transform); // Set the parent of the slot
+
+                slots[i] = newSlot; // Set the array of the slot 
+
+                itemImages[i] = newSlot.transform.GetChild(1).GetComponent<Image>(); // Set the image of the slot
             }
         }
     }
@@ -36,13 +40,17 @@ public class Inventory : MonoBehaviour
         for (int i = 0; i < items.Length; i++)
         {
             if (items[i] != null && items[i].itemType == itemToAdd.itemType && itemToAdd.stackable == true)
-            {
-                // Adding to existing slot
-                items[i].quantity = items[i].quantity + 1;
-                Slot slotScript = slots[i].GetComponent<Slot>();
-                Text quantityText = slotScript.qtyText;
-                quantityText.enabled = true;
-                quantityText.text = items[i].quantity.ToString();
+            { // If the slot already has item
+                
+                items[i].quantity = items[i].quantity + 1; // Add the quantity
+
+                Slot slotScript = slots[i].GetComponent<Slot>(); // Add the slot
+
+                Text quantityText = slotScript.qtyText; // Get the quantity text
+
+                quantityText.enabled = true; // Enable quantity
+
+                quantityText.text = items[i].quantity.ToString(); // Enable text of quantity
                 return true;
             }
 
@@ -50,10 +58,14 @@ public class Inventory : MonoBehaviour
             {
                 // Adding to empty slot
                 // Copy item and add to inventory. Copying so we dont modify original Scriptable Object
-                items[i] = Instantiate(itemToAdd);
-                items[i].quantity = 1;
-                itemImages[i].sprite = itemToAdd.sprite;
-                itemImages[i].enabled = true;
+                items[i] = Instantiate(itemToAdd); // Create the item
+
+                items[i].quantity = 1; // Create the quantity
+
+                itemImages[i].sprite = itemToAdd.sprite; // Add the sprite
+
+                itemImages[i].enabled = true; // Enable the image
+
                 return true;
             }
         }
