23836443748832696b3b30faf7b51ac2a7a26f5a
Ouyang1887415157 <1347585006@qq.com>
2025-02-13 10:51:29 +0800

    health bar completed

--- Assets/Scripts/MonoBehaviors/Character.cs
+++ Assets/Scripts/MonoBehaviors/Character.cs
@@ -3,7 +3,9 @@
 public abstract class Character : MonoBehaviour
 {
 
-    public int hitPoints; // The health points of the character
+    public HitPoints hitPoints; // The health points of the character
 
-    public int maxHitPoints; // The max health points of the character
+    public float maxHitPoints; // The max health points of the character
+
+    public float startingHitPoints; // The starting hit points of the character
 }
--- Assets/Scripts/MonoBehaviors/Player.cs
+++ Assets/Scripts/MonoBehaviors/Player.cs
@@ -2,6 +2,20 @@
 
 public class Player : Character
 {
+
+    public HealthBar healthBarPrefab;
+
+    HealthBar healthbar;
+
+    private void Start()
+    {
+        hitPoints.value = startingHitPoints;
+
+        healthbar = Instantiate(healthBarPrefab);
+
+        healthbar.character = this;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("CanBePickedUp"))
@@ -11,33 +25,52 @@ public class Player : Character
 
             if(hitObject != null)
             {
+                bool shouldDisappear = false;
 
                 print("Hit:" + hitObject.objectName); // Print the name of the object
                 
                 switch(hitObject.itemType)
                 {
                     case Item.ItemType.COIN: // If the object is a coin
+
+                        shouldDisappear = true;
                         break;
 
                     case Item.ItemType.HEALTH: // If the object is a health
-                        AdjustHitPoints(hitObject.quantity);
+
+                        shouldDisappear = AdjustHitPoints(hitObject.quantity);
+
                         break;
 
                     default:
                         break;
 
                 }
-                collision.gameObject.SetActive(false);
+
+                if (shouldDisappear)
+                {
+                    collision.gameObject.SetActive(false);
+                }
+                
             }
 
         }
     }
 
-    public void AdjustHitPoints(int amount) // Adjust the health of the player
+    public bool AdjustHitPoints(int amount) // Adjust the health of the player
     {
-        hitPoints = hitPoints + amount;
+        if(hitPoints.value < maxHitPoints) // If the player does not have max hit points
+        {
+            hitPoints.value = hitPoints.value + amount;
 
-        print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints);
+            print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints.value);
+
+            return true;
+        }
+
+        return false; // if the player already has maxHitPoints
+
+        
     }
 
 }
