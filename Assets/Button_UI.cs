86a3506f775c5a940fd3b4f10192e36bb22a27fa
vilinie <viliniesingh@gmail.com>
2025-02-21 01:01:33 -0400

    Created start menu
    added background, title panel and start button with functionality

---
+++ Assets/Button_UI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_UI : MonoBehaviour
+{
+    [SerializeField] private string newLevel = "TestMap - Pall";
+    
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene(newLevel);
+    }
+
+}
